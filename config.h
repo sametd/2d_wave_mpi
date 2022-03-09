c3b6967a690bb29c44fc346e9a976a608234fa86
Samet Demir <sametdemir@gmail.com>
2022-03-10 02:57:02 +0300

    better parameters

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
-#define dx 0.02
-#define dt 0.001
+#define dx 0.006
+#define dt 0.0001
 // Gauss Seidel off diag
 #define off_diag ((0.25) * (dt * dt) / (dx * dx))
 // Gauss Seidel on diag
 #define on_diag (1 + (4 * off_diag))
-#define f_to_go (3.14159) / 2
+#define f_to_go (3.14159)
 #define grid_size ((int)(f_to_go / dx))
-#define t_to_go 1.5
+#define t_to_go 0.2
 #define gerr_max 1e-10
 
 #define write_posix 0
